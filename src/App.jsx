4b313582da0f53f278e7eef1b4fd521d5c85a1aa
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2025-09-29 23:21:05 +0700

    Add Credits Page , Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,13 @@ import "./App.css";
 import "./assets/prism.css";
 
 import { assets } from "./assets/assets";
+import { useAppContext } from "./context/AppContext";
+import Login from "./pages/Login";
 
 function App() {
+
+  const {user} = useAppContext(); 
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const {pathname} = useLocation();
 
@@ -21,7 +26,9 @@ function App() {
     {!isMenuOpen && <img
     onClick={()=>setIsMenuOpen(true)}
     src={assets.menu_icon} className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden invert dark:invert-0" alt=""/>}
-      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+     
+      {user ? (
+         <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
         <div className="flex h-screen w-screen">
           <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
           <Routes>
@@ -31,6 +38,12 @@ function App() {
           </Routes>
         </div>
       </div>
+      ) :(
+           <div className="bg-gradient-to-b from-[#242124] to-[#000000] h-screen w-screen flex items-center justify-center">
+            <Login/>
+           </div>
+      )}
+     
     </>
   );
 }
